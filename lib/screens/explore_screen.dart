cc582f84503be3870fc393027c793c950447a7a2
Harish2915 <harish29012004@gmail.com>
2025-03-12 15:33:40 +0530

    Add files via upload

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class ExploreScreen extends StatelessWidget {
+  static const routeName = 'explore-screen';
+
+  const ExploreScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.black,
+        child: Column(
+          children: [
+            Container(
+              child: Lottie.network(
+                  'https://assets7.lottiefiles.com/packages/lf20_RYmOmk.json',
+                  repeat: true),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text('Explore the\n     Space',
+                style: TextStyle(color: Colors.white, fontSize: 36)),
+          ],
+        ),
+      ),
+    );
+  }
+}
